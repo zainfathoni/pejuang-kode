2484795768654a43b0e7d694f63fb18abdcd81dd
Zain Fathoni <zain.fathoni@gmail.com>
2018-05-02 02:34:53 +0800

    Add Header styling

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,18 +3,26 @@ import styled from 'styled-components'
 
 export const Main = styled.main`
   max-width: 740px;
-  padding-left: 20px;
-  padding-right: 20px;
+  padding: 0 20px;
   margin: 0 auto;
 `
 
 export const Header = styled.header`
+  background-image: linear-gradient(
+    150deg,
+    rgb(10, 180, 77) 10%,
+    rgb(9, 179, 175) 70%,
+    rgb(9, 179, 175) 94%
+  );
   h1,
   h3 {
     max-width: 740px;
-    padding-left: 20px;
-    padding-right: 20px;
+    padding: 20px;
     margin: 0 auto;
+    a {
+      color: #fff;
+      text-decoration: none;
+    }
   }
 `
 
