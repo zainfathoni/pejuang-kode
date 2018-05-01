3c7ef7e7128098dae662e267830724dd8b5854fc
Zain Fathoni <zain.fathoni@gmail.com>
2018-05-02 03:01:07 +0800

    Whitelabel metadata & favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
+    title: `Pejuang Kode`,
+    author: `Zain Fathoni`,
   },
   plugins: [
     {
