abf176fe36e5f737d34449f988394de74313e19c
Zain Fathoni <zain.fathoni@gmail.com>
2018-05-02 00:54:03 +0800

    Use Titillium Web font

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,5 +1,15 @@
 import Typography from 'typography'
 
-const typography = new Typography({ baseFontSize: '18px' })
+const typography = new Typography({
+  baseFontSize: '18px',
+  googleFonts: [
+    {
+      name: 'Titillium Web',
+      styles: ['400'],
+    },
+  ],
+  headerFontFamily: ['Titillium Web', 'sans-serif'],
+  bodyFontFamily: ['Titillium Web', 'sans-serif'],
+})
 
 export default typography
