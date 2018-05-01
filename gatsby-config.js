5e7546749dfa70d005beb501c43e0640905e3234
Zain Fathoni <zain.fathoni@gmail.com>
2018-05-02 00:47:38 +0800

    Use gatsby-plugin-typography

--- gatsby-config.js
+++ gatsby-config.js
@@ -43,5 +43,11 @@ module.exports = {
     },
     `gatsby-plugin-offline`,
     `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography.js`,
+      },
+    },
   ],
 }
