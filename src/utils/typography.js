abadbd8ae2119ece61d49f5cb7bf4987a683bed5
Zain Fathoni <zain.fathoni@gmail.com>
2018-05-05 02:49:25 +0800

    Typography tweaking

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,15 +1,19 @@
 import Typography from 'typography'
 
 const typography = new Typography({
-  baseFontSize: '18px',
+  baseFontSize: '16px',
   googleFonts: [
+    {
+      name: 'Nunito',
+      styles: ['400'],
+    },
     {
       name: 'Titillium Web',
       styles: ['400'],
     },
   ],
   headerFontFamily: ['Titillium Web', 'sans-serif'],
-  bodyFontFamily: ['Titillium Web', 'sans-serif'],
+  bodyFontFamily: ['Nunito', 'sans-serif'],
 })
 
 export default typography
