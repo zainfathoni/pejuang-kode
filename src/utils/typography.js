97d7a7386d58895ea2d0168f5f08a42d96b56d16
Zain Fathoni <zain.fathoni@gmail.com>
2018-05-05 23:59:33 +0800

    Increase boldWeight for body font

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -5,13 +5,15 @@ const typography = new Typography({
   googleFonts: [
     {
       name: 'Nunito',
-      styles: ['400'],
+      styles: ['400', '800'],
     },
     {
       name: 'Titillium Web',
       styles: ['700'],
     },
   ],
+  headerWeight: '700',
+  boldWeight: '800',
   headerFontFamily: ['Titillium Web', 'sans-serif'],
   bodyFontFamily: ['Nunito', 'sans-serif'],
 })
