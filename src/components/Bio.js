c76bb3ac006ed78eb3c0909f2208a219c5d252ad
Zain Fathoni <zain.fathoni@gmail.com>
2018-05-09 02:09:13 +0800

    Refactor Bio to functional components

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -19,20 +19,16 @@ const Details = styled.div`
   margin-left: 15px;
 `
 
-class Bio extends React.Component {
-  render() {
-    return (
-      <StyledSection>
-        <Avatar size={100} email="zain.fathoni@gmail.com" />
-        <Details>
-          Written by <strong>Zain Fathoni</strong>, a father, husband, and
-          software engineering enthusiast who lives and works in Singapore.
-          <br />You could reach him on&nbsp;
-          <a href="https://t.me/zainfathoni">Telegram</a>.
-        </Details>
-      </StyledSection>
-    )
-  }
-}
+const Bio = props => (
+  <StyledSection>
+    <Avatar size={100} email="zain.fathoni@gmail.com" />
+    <Details>
+      Written by <strong>Zain Fathoni</strong>, a father, husband, and software
+      engineering enthusiast who lives and works in Singapore.
+      <br />You could reach him on&nbsp;
+      <a href="https://t.me/zainfathoni">Telegram</a>.
+    </Details>
+  </StyledSection>
+)
 
 export default Bio
